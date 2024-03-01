2a1dafcbc4efc3d71b82a1f55c73cf1e09090127
andrea-livornese <andrealivornese@gmail.com>
2024-03-01 18:25:27 +0100

    first commit

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,29 @@
+<script>
+
+export default{
+    name:"AppTitle",
+
+    data(){
+        return{
+
+        }
+    }
+}
+
+</script>
+
+<template>
+
+    <h1>
+        La mia prima app con Vite!
+    </h1>
+ 
+</template>
+
+<style>
+
+    h1{
+        text-align: center;
+    }
+
+</style>
